f69108552d614196d5f226394cab1fd9c195cd4b
Alex <vagiwnas@gmail.com>
2020-04-15 17:54:38 +0300

    refactor: Change variable df_sub to df_part

--- bikeshare.py
+++ bikeshare.py
@@ -225,7 +225,7 @@ def get_rand_rows(df, n):
         (DataFrame) df - Pandas DataFrame containing city data filtered by month and day
         (int) n - number of rows to be extracted
     Returns:
-        (DataFrame) df_sub - Pandas DataFrame containing only the exctracted rows
+        (DataFrame) df_part - Pandas DataFrame containing only the exctracted rows
     """
 
     # get total number of rows in DataFrame
@@ -235,22 +235,22 @@ def get_rand_rows(df, n):
     idx_list = [rnd.randint(0, nrows - 1) for i in range(n)]
 
     # extract n random rows from DataFrame
-    df_sub = df.iloc[idx_list]
+    df_part = df.iloc[idx_list]
 
-    return df_sub
+    return df_part
 
 
 def individual_data(df):
     """Displays individual data of bikeshare users."""
 
     # get 5 random rows from DataFrame
-    df_sub = get_rand_rows(df, 5)
+    df_part = get_rand_rows(df, 5)
 
     # remove 1st columns
-    df_sub = df_sub.drop(df.columns[0], axis=1)
+    df_part = df_part.drop(df.columns[0], axis=1)
 
     # display extracted rows
-    for row in df_sub.iterrows():
+    for row in df_part.iterrows():
         pprint(row[1].to_dict())
 
 
